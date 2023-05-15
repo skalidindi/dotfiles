c684a424c5559f60b80c1a81dd0a33ea7d92b0b1
Santosh Kalidindi <skalidindi8@gmail.com>
2023-05-15 15:18:14 -0700

    show hidden files for neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -6,7 +6,7 @@ return {
         visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        -- hide_gitignored = true,
         hide_by_name = {
           '.git',
           '.DS_Store',
