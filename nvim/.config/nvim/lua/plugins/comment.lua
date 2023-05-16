2c281141272163c576be588c1ac402b9d3297955
Santosh Kalidindi <skalidindi8@gmail.com>
2023-05-15 20:03:59 -0700

    Add comment plugin

---
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,24 @@
+return {
+  "numToStr/Comment.nvim",
+  event = "BufReadPost",
+  config = function()
+    require("Comment").setup({
+      padding = true,
+      sticky = true,
+      toggler = {
+        line = "gcc",
+        block = "gbc",
+      },
+      opleader = {
+        line = "gc",
+        block = "gb",
+      },
+      mappings = {
+        basic = true,
+        extra = true,
+        extended = false,
+      },
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
+}
