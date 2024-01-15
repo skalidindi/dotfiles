461ba27ef7b22e1a9b2f2a7c68516b34226d3090
Santosh Kalidindi <skalidindi8@gmail.com>
2024-01-15 15:14:56 -0800

    add treesj

---
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,30 @@
+return {
+  'Wansmer/treesj',
+  keys = {
+    {
+      "<leader>gm",
+      function()
+        require('treesj').toggle()
+      end,
+      desc = "Toggle Split / Join Block",
+    },
+    {
+      "<leader>gj",
+      function()
+        require('treesj').join()
+      end,
+      desc = "Join Block",
+    },
+    {
+      "<leader>gs",
+      function()
+        require('treesj').split()
+      end,
+      desc = "Split Block",
+    }
+  },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({ use_default_keymaps = false })
+  end,
+}
