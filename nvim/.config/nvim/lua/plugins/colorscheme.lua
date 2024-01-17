ec0ff7d3b19a5da1d102ced77e6aca0266ec89ea
Santosh Kalidindi <skalidindi8@gmail.com>
2024-01-17 11:23:50 -0800

    add new colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,4 +9,20 @@ return {
       },
     },
   },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  }
 }
