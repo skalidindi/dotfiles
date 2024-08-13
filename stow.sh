3785b60c235111a8155925ab939af250a9a09523
Santosh Kalidindi <skalidindi8@gmail.com>
2024-08-13 14:21:26 -0700

    chore: remove yabai

--- stow.sh
+++ stow.sh
@@ -1,4 +1,4 @@
-STOW_DIRS=( aerospace alacritty bash bat bin btop fastfetch gh git kitty misc nvim skhd tmux yabai zsh )
+STOW_DIRS=( aerospace alacritty bash bat bin btop fastfetch gh git kitty misc nvim skhd tmux zsh )
 for i in "${STOW_DIRS[@]}"
 do
 	stow -t ~/ -R "$i"
