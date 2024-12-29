5f490525fed742459f6c6dc6679b0a3d31b84722
Santosh Kalidindi <skalidindi8@gmail.com>
2024-12-29 07:21:50 +0530

    simplify nvim setup

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,5 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  event = "VeryLazy",
   opts = {
     options = {
       hover = {
--- nvim/.config/nvim/lua/plugins/notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  config = function(_, opts)
-    local notify = require("notify")
-    opts.background_colour = "#000000"
-    notify.setup(opts)
-  end,
-}
